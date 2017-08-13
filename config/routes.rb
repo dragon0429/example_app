f6fc7869c1b2d94f2bc19a9e5f95c4c750378fc6
dragon0429 <dragon0429@yahoo.com>
2017-08-14 09:25:19 +1000

    mailer

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
-  get '/help',   to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
-  get '/contact',   to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   delete '/users/:id', to: 'users#destory'
   resources 'users'
+  resources :account_activations, only: [:edit]
 end
