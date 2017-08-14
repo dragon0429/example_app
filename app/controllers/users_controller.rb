6a1a481f497af8d426c7428122439109e2415505
dragon0429 <dragon0429@yahoo.com>
2017-08-14 22:20:16 +1000

    Password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def new
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    redirect_to root_url and return unless FILL_IN
+    @user = User.find(params[:id])
+    redirect_to root_url
   end
 
   def edit
@@ -39,8 +40,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      flash[:succeess] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
@@ -12,6 +16,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   delete '/users/:id', to: 'users#destory'
-  resources 'users'
+  resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
 end
